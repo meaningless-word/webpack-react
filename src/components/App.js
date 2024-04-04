e9b355a5d528305b8d6a633f1748b0ee25fa01ed
meaningless-word <grisly@mail.ru>
2024-04-04 22:35:28 +0700

    замена классового представления компонента на функциональный

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,29 @@ import React from "react";
 import "../styles/App.css";
 import Header from "./Header";
 
-let buttonName = "Any key";
+// переход от классового представления к фукнциональному
+function App() {
+  let buttonName = "Any key";
+  let [count, incrementCount] = React.useState(0);
 
-const handleClick = () => {
-  console.log("Clicked");
-};
+  const handleClick = (e) => {
+    incrementCount(count + 1);
+    console.log(e.target);
+  };
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <main>
-          <div>
-            <h1 className="in-main">Press any key</h1>
-            <button clasName={"some-button"} onClick={handleClick}>
-              {buttonName}
-            </button>
-          </div>
-        </main>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <div>
+          <h1 className="in-main">Press any key</h1>
+          <button clasName={"some-button"} onClick={handleClick}>
+            {buttonName} претерпела {count} нажатий
+          </button>
+        </div>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
